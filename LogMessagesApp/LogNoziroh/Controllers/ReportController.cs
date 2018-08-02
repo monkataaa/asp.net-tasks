0a1e86834590fd0d2ad6728893db37a0d1cbeb9a
monkataaa <monkataaa@abv.bg>
2018-08-02 22:54:06 +0300

    log messages app - initial upload

---
+++ LogMessagesApp/LogNoziroh/Controllers/ReportController.cs
@@ -0,0 +1,83 @@
+﻿namespace LogNoziroh.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Models;
+
+    [ValidateInput(false)]
+    public class ReportController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var context = new LogNozirohDbContext())
+            {
+                List<Report> reports = context.Reports.ToList();
+
+                return View(reports);
+            }
+        }
+
+        [HttpGet]
+        [Route("details/{id}")]
+        public ActionResult Details(int id)
+        {
+            using (var db = new LogNozirohDbContext())
+            {
+                var report = db.Reports.Find(id);
+
+                return View(report);
+            }
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Report report)
+        {
+            using (var context = new LogNozirohDbContext())
+            {
+                context.Reports.Add(report);
+                context.SaveChanges();
+
+                return this.Redirect("/");
+            }
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            using (var db = new LogNozirohDbContext())
+            {
+                var report = db.Reports.Find(id);
+               
+                return View(report);
+            }
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id, Report reportModel)
+        {
+            using (var db = new LogNozirohDbContext())
+            {
+                var report = db.Reports.Find(id);
+              
+                db.Reports.Remove(report);
+                db.SaveChanges();
+                return this.Redirect("/");
+            }
+        }
+    }
+}
